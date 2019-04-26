ef73b2dbee28a3f5062fa27992efa073e0015190
Erica <erikamoisei@gmail.com>
2019-04-26 11:24:07 -0300

    Done

---
+++ ArrayListsStructuresEx/ArrayListsStructuresEx/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ArrayListsStructuresEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFindGrade = new System.Windows.Forms.Button();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFindGrade
+            // 
+            this.btnFindGrade.Location = new System.Drawing.Point(185, 116);
+            this.btnFindGrade.Name = "btnFindGrade";
+            this.btnFindGrade.Size = new System.Drawing.Size(75, 23);
+            this.btnFindGrade.TabIndex = 0;
+            this.btnFindGrade.Text = "&Find grade";
+            this.btnFindGrade.UseVisualStyleBackColor = true;
+            this.btnFindGrade.Click += new System.EventHandler(this.btnFindGrade_Click);
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(175, 59);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 1;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(80, 62);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(38, 13);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 196);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.btnFindGrade);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFindGrade;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ ArrayListsStructuresEx/ArrayListsStructuresEx/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayListsStructuresEx
+{
+    //Erica Moisei -- Apr. 26/2019
+    public partial class Form1 : Form
+    {
+        ArrayList gradesScale = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //adding the range
+            gradesScale.Add(299);
+            gradesScale.Add("F");
+            gradesScale.Add(349);
+            gradesScale.Add("D");
+            gradesScale.Add(399);
+            gradesScale.Add("C");
+            gradesScale.Add(449);
+            gradesScale.Add("B");
+            gradesScale.Add(500);
+            gradesScale.Add("A");
+        }
+
+        private void btnFindGrade_Click(object sender, EventArgs e)
+        {
+            int score = Convert.ToInt32(txtScore.Text);
+            
+            if (score >= 0 && score <= Convert.ToInt32(gradesScale[0]))
+            {
+                MessageBox.Show("The grade is F");
+            }
+            else if (score <= Convert.ToInt32(gradesScale[2]))
+            {
+                MessageBox.Show("The grade is D");
+            }
+            else if (score <= Convert.ToInt32(gradesScale[4]))
+            {
+                MessageBox.Show("The grade is C");
+            }
+            else if (score <= Convert.ToInt32(gradesScale[6]))
+            {
+                MessageBox.Show("The grade is B");
+            }
+            else if (score <= Convert.ToInt32(gradesScale[8]))
+            {
+                MessageBox.Show("The grade is A");
+            }
+            else
+            {
+                MessageBox.Show("The score is out of the grade range");
+            }
+        }
+
+    }
+}
